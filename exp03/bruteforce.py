14a23e3535d0e87b7059fc953b7a10646c349796
Guilherme S. Mazzariol <gsmazzariol@gmail.com>
2017-10-04 23:30:44 -0300

    [exp03] add getopt arguments to bruteforce.py

--- exp03/bruteforce.py
+++ exp03/bruteforce.py
@@ -1,4 +1,7 @@
+import getopt
 from zipfile import ZipFile
+
+import sys
 
 __author__ = 'guilherme.mazzariol && marcelo'
 
@@ -21,7 +24,13 @@ def read_file(src_file):
         return lines
 
 
-def extract_data(zip_file, list_pwd):
+def access_zip_file_with_password(zip_file, list_pwd):
+    """
+    Try to discorvery a zip file password from a list
+    :param zip_file: source for a zip file
+    :param list_pwd: list of string
+    :return: string
+    """
     password = ''
     try:
         with ZipFile(zip_file) as zf:
@@ -40,11 +49,30 @@ def extract_data(zip_file, list_pwd):
     finally:
         return password
 
+def main(argv):
+    source_list = ''
+    zip_filename = ''
+    try:
+        opts, args = getopt.getopt(argv, "h:l:f", ["help, list, file"])
+    except getopt.GetoptError as e:
+        print('bruteforce -l /tmp/list.txt -f /tmp/secrets.zip. ')
+        sys.exit(2)
+    else:
+        for opt, arg in opts:
+            if opt == '-h':
+                print('help')
+                sys.exit()
+            elif opt in ('-l', '--list'):
+                source_list = arg
+            elif opt in ('-f', '--file'):
+                zip_filename = arg
+            else:
+                print('Unknown parameter!\n')
+                sys.exit(2)
+        lines = read_file(source_list)
+        pwd = access_zip_file_with_password(zip_filename, lines)
+        print('The password is {0}'.format(pwd))
 
 if __name__ == '__main__':
-    src = '/home/guilherme/teste/t1.txt'
-    zip = '/home/guilherme/teste/segc.zip'
-    lines = read_file(src)
-    pwd = extract_data(zip, lines)
-    print('The password is {0}'.format(pwd))
+    main(sys.argv)
 
