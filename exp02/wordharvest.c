e14d0467dc27fb8b926905e18607d7d2174a2783
Guilherme S. Mazzariol <gsmazzariol@gmail.com>
2017-10-15 11:33:04 -0200

    [wordharvest] add funcao que le todo os parametros.

--- exp02/wordharvest.c
+++ exp02/wordharvest.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
-int main()
-{
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+void search_words(){
+    // search for files with the extensions .txt, .text and .asc;
+    // start the search from directory /tmp/;
+    // save the words found in the file words_tmp.
+}
+
+// CHAMADA: wordharvest -e txt:text:asc -d /tmp/ -o words_tmp
+int main(int argc, char **argv) {
+    char *eflag = NULL;
+    char *dflag = NULL;
+    char *ovalue = NULL;
+    int index;
+    int c;
+
+    opterr = 0;
+
+    while ((c = getopt (argc, argv, "e:d:o:")) != -1)
+        switch (c)
+        {
+            case 'e':
+                eflag = optarg;
+                break;
+            case 'd':
+                dflag = optarg;
+                break;
+            case 'o':
+                ovalue = optarg;
+                break;
+            case '?':
+                if (optopt == 'c')
+                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf (stderr,
+                             "Unknown option character `\\x%x'.\n",
+                             optopt);
+                return 1;
+            default:
+                abort ();
+        }
+
+    printf ("eflag = %s, dflag = %s, ovalue = %s\n",
+            eflag, dflag, ovalue);
+
+    for (index = optind; index < argc; index++)
+        printf ("Non-option argument %s\n", argv[index]);
+    return 0;
 }
