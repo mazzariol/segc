60ccce53bf57e6331947b14909eed1888a6c1d97
Guilherme S. Mazzariol <gsmazzariol@gmail.com>
2017-10-15 12:37:51 -0200

    [wordharvest] read extensions ok

--- exp02/wordharvest.c
+++ exp02/wordharvest.c
@@ -2,18 +2,40 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
-void search_words(){
+void search_words(char *e){
+    // OK get params
     // search for files with the extensions .txt, .text and .asc;
     // start the search from directory /tmp/;
     // save the words found in the file words_tmp.
 }
 
+char ** parse_string(char *str){
+    char ** res  = NULL;
+    char * p = strtok (str, ":");
+    int n_spaces = 0;
+
+    /* split string and append tokens to 'res' */
+    while (p) {
+        res = realloc (res, sizeof (char*) * ++n_spaces);
+        if (res == NULL)
+            exit (-1); /* memory allocation failed */
+        res[n_spaces-1] = p;
+        p = strtok (NULL, ":");
+    }
+    /* realloc one extra element for the last NULL*/
+    res = realloc (res, sizeof (char*) * (n_spaces+1));
+    res[n_spaces] = 0;
+
+    return res;
+}
 // CHAMADA: wordharvest -e txt:text:asc -d /tmp/ -o words_tmp
 int main(int argc, char **argv) {
-    char *eflag = NULL;
-    char *dflag = NULL;
-    char *ovalue = NULL;
+    char * eflag = NULL;
+    char * dflag = NULL;
+    char * ovalue = NULL;
+    char ** extensions = NULL;
     int index;
     int c;
 
@@ -45,9 +67,13 @@ int main(int argc, char **argv) {
                 abort ();
         }
 
+
     printf ("eflag = %s, dflag = %s, ovalue = %s\n",
             eflag, dflag, ovalue);
-
+    extensions = parse_string(eflag);
+    /* print the result
+        for (i = 0; i < n_spaces; ++i)
+            printf ("res[%d] = %s\n", i, res[i]);*/
     for (index = optind; index < argc; index++)
         printf ("Non-option argument %s\n", argv[index]);
     return 0;
