d7a45523cdb7a20809753cb6ecd04e32154c2626
Guilherme S. Mazzariol <gsmazzariol@gmail.com>
2017-10-15 01:16:36 -0200

    [finish] trabalho finalizado

---
+++ exp03/README.txt
@@ -0,0 +1,8 @@
+# Computer Security - 2017s2 - EXP03
+    Guilherme Mazzariol RA  138466
+    Marcelo Machado     RA  210402
+---------------------------------------
+Para executar o programa, siga os seguintes passo:
+
+$ chmod +x bruteforce.py
+$ ./bruteforce.py -l list2.txt -f secrets.zip
--- exp03/bruteforce.py
+++ exp03/bruteforce.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python
+
+import sys
 import getopt
 from zipfile import ZipFile
 
-import sys
-
-__author__ = 'guilherme.mazzariol && marcelo'
+__author__ = 'guilherme.mazzariol-RA-138466 && marcelo.machado-RA-210402'
 
 
 def read_file(src_file):
@@ -18,8 +19,8 @@ def read_file(src_file):
         file = open(src_file, 'r')
         lines = file.read().splitlines()
         file.close()
-    except Exception as e:
-        print(e)
+    except Exception:
+        pass
     finally:
         return lines
 
@@ -34,45 +35,42 @@ def access_zip_file_with_password(zip_file, list_pwd):
     password = ''
     try:
         with ZipFile(zip_file) as zf:
-            data = 'Bad password for file'
-            while list_pwd or data is not None:
+            count = len(list_pwd) - 1
+            while count > 0 and password == '':
+                count -= 1
                 try:
+                    zf.extractall(path='/tmp/', pwd=list_pwd[0].encode('utf-8'))
                     password = list_pwd[0]
-                    data = zf.extractall(path='/tmp/', pwd=password.encode('utf-8'))
-                    list_pwd.remove(password)
-                except Exception as e:
-                    list_pwd.remove(password)
-                    data = e.args[0]
+                except Exception:
+                    list_pwd.remove(list_pwd[0])
         zf.close()
-    except Exception as e:
-        print(e)
+    except Exception:
+        pass
     finally:
         return password
+
 
 def main(argv):
     source_list = ''
     zip_filename = ''
     try:
-        opts, args = getopt.getopt(argv, "h:l:f", ["help, list, file"])
-    except getopt.GetoptError as e:
-        print('bruteforce -l /tmp/list.txt -f /tmp/secrets.zip. ')
+        opts, args = getopt.getopt(argv, "hl:f:v", ["help, list, file"])
+    except getopt.GetoptError:
         sys.exit(2)
     else:
-        for opt, arg in opts:
-            if opt == '-h':
-                print('help')
-                sys.exit()
-            elif opt in ('-l', '--list'):
-                source_list = arg
-            elif opt in ('-f', '--file'):
-                zip_filename = arg
-            else:
-                print('Unknown parameter!\n')
-                sys.exit(2)
-        lines = read_file(source_list)
-        pwd = access_zip_file_with_password(zip_filename, lines)
-        print('The password is {0}'.format(pwd))
+        if len(opts) == 2:
+            for opt, arg in opts:
+                if opt in ('-l', '--list'):
+                    source_list = arg
+                elif opt in ('-f', '--file'):
+                    zip_filename = arg
+                else:
+                    print('Unknown parameter!\n')
+                    sys.exit(2)
+            lines = read_file(source_list)
+            pwd = access_zip_file_with_password(zip_filename, lines)
+            if len(pwd) > 0:
+                print('The password is {0}'.format(pwd))
 
 if __name__ == '__main__':
-    main(sys.argv)
-
+    main(sys.argv[1:])
