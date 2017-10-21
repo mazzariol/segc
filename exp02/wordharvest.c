2f7511a9ae7f52d31d60c3c1361eb8aa66b48305
Guilherme S. Mazzariol <gsmazzariol@gmail.com>
2017-10-21 18:39:00 -0200

    [exp02] update projetct

--- exp02/wordharvest.c
+++ exp02/wordharvest.c
@@ -1,102 +1,95 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h> /* malloc */
+#include <string.h> /* strlen, strcpy, strncpy */
+#include <ctype.h> /* isalpha, isdigit */
+#include <unistd.h> /* getopt */
 
-// OK   get params
-// OK   search for files with the extensions .txt, .text and .asc;
-// OK   start the search from directory /tmp/;
-// save the words found in the file words_tmp
+/**
+ * Marcelo Machado      - RA 210402
+ * Guilherme Mazzariol  - RA 138466
+ *
+ * Given a directory and some extensions, get recursively files with that extensions
+ * inside the directory. For each files, get 'words', beings 'word' defined by a
+ * sequence of letters + numbers. Every other char is considered a separator.
+ *
+ * If no extensions are given, .txt and .text should be considered.
+ * Only new words should be added to the list.
+ */
 
-char *tmp_file = "/tmp/out.log";
+// define max size for words
+#define MAX 999
 
-char ** read_data_from_file(const char *filename);
-char* concat(const char *s1, const char *s2);
-void search_files(char **extensions, char *dir);
+// linked list definition
+typedef struct node {
+    char word[MAX];
+    struct node *next;
+} Node;
+
+// global variables
+Node* l_words;
+
+// utilities
 char ** parse_string(char *str, char *delimiter);
-char ** read_words_from_file(const char *filename);
+char* concat(const char *s1, const char *s2);
 
-char ** read_words_from_file(const char *filename){
-    char ** lines = NULL;
-    FILE *fp;
-    int ch;
+// important functions
+void find_files_in_dir(char *dir, char *ext);
+void find_words_in_file(char *filename);
 
-    fp = fopen(filename,"r");
-    if(fp == NULL) {
-        perror("Error in opening file");
-        return NULL;
+// list functions
+void list_to_file(Node* l, char *output_filename);
+void push(Node* l, char *word);
+
+// main function
+int main (int argc, char **argv) {
+    int opt, eflag = 0;
+    char *working_dir = NULL;
+    char *extensions = NULL;
+    char *output_filename = NULL;
+
+    // init list
+    l_words = NULL;
+    l_words = malloc(sizeof(Node));
+
+    // parse arguments: extensions, working dir, output filename
+    while ((opt = getopt(argc, argv, "d:e:o:")) != -1) {
+        switch(opt) {
+        case 'd':
+            working_dir = optarg;
+            break;
+        case 'e':
+            eflag = 1;
+            extensions = optarg;
+            break;
+        case 'o':
+            output_filename = optarg;
+            break;
+        default:
+            abort();
+        }
     }
-    while((ch = (char)fgetc(fp) ) != EOF) {
-        if( feof(fp)) {
-            break ;
-        }
-        // Consider char between 0-9 || A-Z || a-z
-        if ((48 <= ch && ch <= 57) || (65 <= ch && ch <= 90) || (97 <= ch && ch <= 122)){
-            printf("%c", ch);
-        } else {
-            printf("\n");
-        }
+
+    // extensions not provided: use default extensions
+    if (!eflag) {
+        extensions = (char*)malloc(9*sizeof(char));
+        strcpy(extensions, "txt:text");
     }
 
-    fclose(fp);
-
-    return lines;
-}
-
-char ** read_data_from_file(const char *filename){
-    sleep(1);
-    char info[50];
-    FILE *arq;
-
-    arq = fopen(filename, "r");
-    if(arq == NULL)
-        printf("Erro, nao foi possivel abrir o arquivo\n");
-    else
-        while( (fgets(info, sizeof(info), arq))!=NULL ) {
-            printf("%s", info);
-            if (info[strlen(info) -1] == '\n')
-                info[strlen(info) -1] = '\0';
-            read_words_from_file(info);
-        }
-
-    fclose(arq);
+    find_files_in_dir(working_dir, extensions);
+    list_to_file(l_words, output_filename);
 
     return 0;
 }
 
-char* concat(const char *s1, const char *s2) {
-    char *result = malloc(strlen(s1)+strlen(s2)+1);
-    strcpy(result, s1);
-    strcat(result, s2);
 
-    return result;
-}
-
-void search_files(char **extensions, char *dir){
-    char *new_str;
-    int i=0;
-    new_str = concat("find ", dir);
-    new_str = concat(new_str, " -regex '.*\\.\\(");
-
-    while (extensions[i] != NULL){
-        new_str = concat(new_str, extensions[i]);
-        new_str = concat(new_str, "\\|");
-        i ++;
-    }
-    new_str = concat(new_str, "\\)'");
-    new_str = concat(new_str, " > ");
-    new_str = concat(new_str, tmp_file);
-
-    // execute 'find' command
-    popen(new_str, "r");
-    free(new_str);
-}
-
-/**
- * Parse string from delimiter
- * */
 char ** parse_string(char *str, char *delimiter){
+    /**
+     * Given a delimiter, parse a string
+
+     * @param char *str string to be parsed
+     * @param char *delimiter
+     * @return array with contains each peace of string
+     */
     char ** res  = NULL;
     char * p = strtok (str, delimiter);
     int n_spaces = 0;
@@ -114,54 +107,161 @@ char ** parse_string(char *str, char *delimiter){
 
     return res;
 }
-// CHAMADA: wordharvest -e txt:text:asc -d /tmp/ -o words_tmp
-int main(int argc, char **argv) {
-    char * eflag = NULL;
-    char * dflag = NULL;
-    char * ovalue = NULL;
-    char ** extensions = NULL;
-    int index;
-    int c;
 
-    opterr = 0;
+char* concat(const char *s1, const char *s2) {
+    /**
+     * Concat two strings
 
-    while ((c = getopt (argc, argv, "e:d:o:")) != -1)
-        switch (c)
-        {
-            case 'e':
-                eflag = optarg;
-                break;
-            case 'd':
-                dflag = optarg;
-                break;
-            case 'o':
-                ovalue = optarg;
-                break;
-            case '?':
-                if (optopt == 'c')
-                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-                else if (isprint (optopt))
-                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-                else
-                    fprintf (stderr,
-                             "Unknown option character `\\x%x'.\n",
-                             optopt);
-                return 1;
-            default:
-                abort ();
+     * @param char *s1 first string
+     * @param char *s1 second string
+     * @return concatenate string
+     */
+    char *result = malloc(strlen(s1)+strlen(s2)+1);
+    strcpy(result, s1);
+    strcat(result, s2);
+
+    return result;
+}
+
+void find_files_in_dir (char *dir, char *ext) {
+    /**
+     * Given a dir, list (recursively) files inside it
+     * by calling external application 'find'.
+
+     * @param char *dir name of the dir where to look at
+     * @param char *ext list of extensions to look at, separated by '|'
+     */
+
+    FILE *fp;
+    char cmd[1024];
+    char path[1035];
+    char ** extensions = parse_string(ext, ":");
+    char *command;
+    int i=0;
+
+    command = concat("find ", dir);
+    command = concat(command, " -regex '.*\\.\\(");
+
+    while (extensions[i] != NULL){
+        command = concat(command, extensions[i]);
+        command = concat(command, "\\|");
+        i ++;
+    }
+    command = concat(command, "\\)'");
+
+    // compose external command to be run
+    snprintf(cmd, sizeof(cmd), command);
+    // run external command
+    fp = popen(cmd,"r");
+    // check for errors
+    if (fp == NULL) {
+        printf("Failed to run command\n");
+        exit(1);
+    }
+
+    // iterate through output of 'find' command
+    while (fgets(path, sizeof(path)-1, fp) != NULL) {
+        path[strlen(path)-1] = 0;
+        // find words in each interesting file
+        find_words_in_file(path);
+    }
+
+    pclose(fp);
+}
+
+void find_words_in_file(char *filename) {
+    /**
+     * Find 'words' (sequences of letters and/or numbers) inside
+     * a file, given its path.
+     *
+     * @param char *filename path of the file where to look at
+     */
+
+    int i;
+    char c;
+    char str[MAX];
+    char word[MAX];
+    FILE *fp;
+
+    fp = fopen(filename, "r");
+    if (fp) {
+        i = 0;
+
+        // read each char in file fp
+        while ((c = getc(fp)) != EOF) {
+            // found a letter or a digit: save to word
+            if (isalpha(c) || isdigit(c)) {
+                str[i] = c;
+                i++;
+            }
+            // found a separator: save word and reset i-counter
+            else {
+                // this condition handles consecutive separators
+                if (i) {
+                    strncpy(word, str, i);
+                    // add string terminator to word
+                    word[i] = '\0';
+                    // add words to list
+                    push(l_words, word);
+                }
+                i = 0;
+            }
         }
+        fclose(fp);
+    }
+}
 
-    extensions = parse_string(eflag, ":");
+void list_to_file(Node* l, char *output_filename) {
+    /**
+     * Write elements from list to file.
+     *
+     * @param Node* l list of strings
+     * @param char *output_filename file to be written
+     */
 
-    search_files(extensions, dflag);
+    FILE *fp;
+    Node* current;
 
-    read_data_from_file(tmp_file);
-    /* print the result
-        for (i = 0; i < n_spaces; ++i)
-            printf ("res[%d] = %s\n", i, res[i]);*/
-    for (index = optind; index < argc; index++)
-        printf ("Non-option argument %s\n", argv[index]);
+    // try to open output file
+    fp = fopen(output_filename, "w");
+    if (fp == NULL) {
+        printf("Failed to write file.\n");
+        exit(1);
+    }
 
-    free(extensions);
-    return 0;
+    // walk through list and write words to file
+    current = l;
+    while (current->next != NULL) {
+        fprintf(fp, "%s\n", current->word);
+        current = current->next;
+    }
+    fclose(fp);
+}
+
+void push(Node *l, char word[]) {
+    /**
+     * Add a new word to list of words.
+     *
+     * @param Node* l list of strings
+     * @param char word[] word to be added
+     */
+
+    Node *current, *previous;
+
+    current = l;
+    previous = NULL;
+    // walk through list to add word at the end
+    while (current != NULL) {
+        previous = current;
+        // word is already in list: do not add it
+        if (strcmp(word, current->word) == 0)
+            return;
+        current = current->next;
+    }
+
+    // add new word to the end of the list
+    previous->next = malloc(sizeof(Node));
+    strcpy(previous->next->word, word);
+    previous->next->next = NULL;
+
 }
